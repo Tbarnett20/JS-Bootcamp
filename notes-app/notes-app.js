2d4dd8c46c2060cba0674d08bc4832b6db56e977
Terrance <tbarnett2020@gmail.com>
2018-08-10 20:36:14 -0400

    Added event listeners to both Notes and Todo app

---
+++ notes-app/notes-app.js
@@ -0,0 +1,58 @@
+// DOM - Document Object Model
+
+const notes = [{
+  title: 'My next trip',
+  body: 'I would like to go to Spain',
+ }, {
+  title: 'Habbits to work on',
+  body: 'Exercise. Eating a bit better',
+ }, {
+  title: 'Office Modification',
+  body: 'Get new computer'
+ }]
+
+document.querySelector('#create-note').addEventListener('click', function (e) {
+  e.target.textContent = "Button was clicked"
+})
+
+
+
+document.querySelector('#remove-all').addEventListener('click', function (e) {
+  document.querySelectorAll('.note').forEach(function (note) {
+    note.remove()
+  })
+})
+
+
+
+
+
+
+
+
+
+
+// // const p = document.querySelector('p')
+// // p.remove()
+
+// // Query all and remove
+
+// const ps = document.querySelectorAll('p')
+// // Removes all P tags
+// // ps.forEach(function (p) {
+// //   p.remove()
+// // })
+
+// // Cobsole logs all P tags
+// ps.forEach(function (p) {
+//   console.log(p.textContent)
+// })
+// //  prints to screen
+// ps.forEach(function (p) {
+//   p.textContent = '*******'
+// })
+
+// // Add a new element
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'This is a new element from JavaScript'
+// document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,9 +1,30 @@
-const ps = document.querySelectorAll('p')
-// Removes all P tags
-ps.forEach(function (p) {
-  // If the text content in the pararagrap (p) contains 'The' remove it
- if (p.textContent.includes('The')) {
-   p.remove()
- }
+const todos = [
+  {text: 'Finish Lambda',
+  completed: false,},
+  {text: 'Search for a job',
+  completed: true,},
+  {text: 'Get a job',
+  completed: true,},
+  {text: 'Move into an apartment',
+  completed: false,},
+  {text:'Get a car',
+  completed: false,}
+]
+
+const incomplete = todos.filter(function (todo) {
+  return !todo.completed
 })
 
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incomplete.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function (todo) {
+  const p = document.createElement('p')
+  p.textContent = todo.text
+  document.querySelector('body').appendChild(p)
+});
+// Listen for new todo creation
+document.querySelector('#add-todo').addEventListener('click', function (e) {
+  console.log('Im adding a todo')
+})
